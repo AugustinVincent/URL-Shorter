7b6bdbf18959357672674435af157cb53359df5a
Victor Heckmann <victorheckmann13@gmail.com>
2021-03-17 16:26:41 +0100

    .

--- views/navbar.php
+++ views/navbar.php
@@ -1,23 +1,28 @@
 
 <nav>
     <div class="container">
-        <a href="home.php">Home</a>
-        <a href="converturl.php">Convert Url</a>
+        <div class="texte">
+           <li><a href="home.php">Home</a><li>
+           <li><a href="converturl.php">Convert Url</a><li>
+           <li><a href="contact.php">Contact</a><li>
+           <li><a href="pricing.php">Pricing</a><li>
 
+        
+        
+            <!-- Display is the user isn't connect and if he's not we also display login and sign up links -->
+            <?php if(empty($_SESSION['username'])) :?>
+                
+                <a href="signup.php"><button>Inscription<button></a>
+                <a href="login.php">Connexion</a>
+            <?php endif ?>
+        <div>
 
-        <!-- Display is the user isn't connect and if he's not we also display login and sign up links -->
-        <?php if(empty($_SESSION['username'])) :?>
-            <p>You are'nt connected</p>
-            <a href="signup.php">Inscription</a>
-            <a href="login.php">Connexion</a>
-        <?php endif ?>
-
-
-        <!-- If the user is connect we just wiish him welcome and display a log out button  -->
+        
+        <!-- If the user is connect we just wish him welcome and display a log out button  -->
         <?php if(isset($_SESSION['username'])) :?>
             <p>Welcome <?=$_SESSION['username']?></p>
             <a href="../../src/component/Logout.php">Logout</a> 
         <?php endif ?>
-
+        
     </div>
 </nav>
