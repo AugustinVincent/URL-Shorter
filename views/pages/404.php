aa1df2b800cac67c2c7512036dfb8273378097a0
Augustin VINCENT <augustin.vincent@hetic.net>
2021-03-18 19:36:36 +0100

    .

---
+++ views/pages/404.php
@@ -0,0 +1,13 @@
+<?php include '../header.php';?>
+    <link rel="stylesheet" href="../../public/css/contact.css">
+    <title>Contact</title>
+</head>
+<body>
+    <?php include '../navbar.php';?>
+
+
+
+    <h1>Oups 404 error ..... WRONG PLACE</h1>
+    
+
+<?php include '../footer.php';?>
