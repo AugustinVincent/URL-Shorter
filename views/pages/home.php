65cd4c3f34605d4ec4d14922068902261121ee78
Augustin VINCENT <augustin.vincent@hetic.net>
2021-03-29 00:13:51 +0200

    web gl bg

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,12 +1,21 @@
     <?php include '../header.php';?>
     <link rel="stylesheet" href="../../public/css/home.css">
     <title>Home page</title>
+    <style>
+        .landing-section .webgl{
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index : -1;
+}
+    </style>
 </head>
 <body>
     <?php include '../navbar.php';?>
 
 
     <section class="landing-section">
+    <canvas class="webgl"></canvas>
         <div class="container">
             <h1 class="main-title">The best<br>Url shortener</h1>
             <div class="btn-container">
@@ -22,6 +31,9 @@
         </div>
     </section>
     </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r126/three.min.js" integrity="sha512-n8IpKWzDnBOcBhRlHirMZOUvEq2bLRMuJGjuVqbzUJwtTsgwOgK5aS0c1JA647XWYfqvXve8k3PtZdzpipFjgg==" crossorigin="anonymous"></script>
+
+    <script src="script.js" ></script>
     
 
 <?php include '../footer.php';?>
---
+++ views/pages/script.js
@@ -0,0 +1,90 @@
+// import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+Textures
+ */
+
+const textureLoader = new THREE.TextureLoader()
+const texture = textureLoader.load('texture.png')
+const disMap = textureLoader.load('displacement-map.png')
+const alphaMap = textureLoader.load('alpha-map.png')
+
+/**
+ * Objects
+ */
+
+const planeGeometry = new THREE.PlaneBufferGeometry(5, 5, 64, 64)
+const planeMaterial = new THREE.MeshStandardMaterial({
+    color : 0xffffff,
+    map : texture,
+    displacementMap : disMap,
+    alphaMap : alphaMap,
+    transparent : true,
+    wireframe : true
+})
+
+const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+console.log(plane)
+plane.rotation.x =  - Math.PI/2.5
+
+scene.add(plane)
+
+/**
+ LIGHTS
+ */
+
+ const pointLight = new THREE.PointLight(0x00baff, 10)
+ scene.add(pointLight)
+ pointLight.position.y = 1
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.set(0,0,4)
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas : canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+let mouseY
+window.addEventListener('mousemove', (e) =>
+{
+    mouseY =  e.clientY
+})
+
+window.addEventListener('resize', () =>
+{
+    sizes.width = window.innerWidth
+    sizes.height =  window.innerHeight
+    renderer.setSize(sizes.width, sizes.height)
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+})
+const animate = () =>
+{
+    plane.rotation.z += 0.005
+    plane.material.displacementScale = 0.5 +mouseY * 0.0005
+    renderer.render(scene, camera)
+    requestAnimationFrame(animate)
+}
+animate()
